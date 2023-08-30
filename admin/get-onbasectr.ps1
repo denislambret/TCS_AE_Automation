3bf360d7067be54988488c2971c41ae93285e282
denis lambret <denis.lambret@gmail.com>
2023-08-30 09:25:42 +0200

    20230830 commit office

--- admin/get-onbasectr.ps1
+++ admin/get-onbasectr.ps1
@@ -8,11 +8,13 @@ write-Host "--------------------------------------------------------------------
 # Count CCM
 $lastDir = Get-ChildItem -Path $ccm_path -Directory | Sort-Object CreationTime -Descending | Select-Object -First 1
 write-Host "CCM directory : " $lastDir
-$countCCMFiles = (gci $lastDir/*.csv).Count
-$countCCM = (gci $lastDir/*.csv  | sort $_.LastWriteDate | gc).Count
+$countCCMFiles = (Get-ChildItem $lastDir/*.csv).Count
+$countCCM = (Get-ChildItem $lastDir/*.csv  | Sort-Object $_.LastWriteDate | gc).Count
 write-Host "CCM Files     : " $countCCMFiles " file(s)"
 write-Host "CCM count     : " $countCCM " record(s)"
 # Count Scan
-$countScan = (gci $scan_path *.csv | sort $_.LastWriteDate | select -last 1 | gc).Count
+$scanFile = Get-ChildItem $scan_path *.csv | Sort-Object $_.LastWriteDate | Select-Object -last 1
+$countScan = (Get-ChildItem $scan_path *.csv | Sort-Object $_.LastWriteDate | Select-Object -last 1 | gc).Count
+write-Host "Scan index    : " ($countScan - 1) 
 write-Host "Scanned docs  : " ($countScan - 1) " record(s)"
 write-Host "----------------------------------------------------------------------------------------------------------"
