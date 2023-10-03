ab46f6bdaf7dd37a11e9c3bf06b561bd9a40e9ab
denis lambret <denis.lambret@gmail.com>
2023-10-03 09:16:02 +0200

    20231003

---
+++ Projects/IDIT_SQLQueries/IDIT_Check status job optimisation index.sql
@@ -0,0 +1,25 @@
+-- Suivi du status d'avancement du job d'indexation table
+-- Author : DLA
+-- Date   : 27.09.2023
+SELECT TOP (1000) [ID],
+	  [StartTime], 
+	  [DatabaseName],
+	  DATEDIFF(MINUTE,[StartTime], 
+	  [EndTime]) AS [Rebuild duration minutes],
+	  [SchemaName],
+	  [ObjectName],
+	  [ObjectType],
+	  [IndexName],
+	  [IndexType],
+	  [StatisticsName],
+	  [PartitionNumber],
+	  [ExtendedInfo],
+	  [Command],
+	  [CommandType],
+	  [EndTime],
+	  [ErrorNumber],
+	  [ErrorMessage]
+  FROM [master].[dbo].[CommandLog] 
+  where CommandType = 'ALTER_INDEX'
+  --order by [Rebuild duration minutes] desc
+	order by [StartTime] desc 
