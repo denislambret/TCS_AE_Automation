163bf4fa0677c7da694c3cf1c87080d39859e0f0
DNSTheMenace <denis.lambret@gmail.com>
2024-03-12 01:03:33 +0100

    20240312

--- Projects/ONBASE_ExtractTransLog/Get-TransactionLogs.ps1
+++ Projects/ONBASE_ExtractTransLog/Get-TransactionLogs.ps1
@@ -2,27 +2,50 @@
 # Script  : Get-TransactionLogs.ps1
 #----------------------------------------------------------------------------------------------------------------------------------
 # Author  : DLA
-# Date    : 20230208
-# Version : 0.1
+# Date    : 20230311
+# Version : 0.2
 #----------------------------------------------------------------------------------------------------------------------------------
 <#
     .SYNOPSIS
-        Retrieve records from transaction table on a daily base on ONBASE db
+        Retrieve records from transaction table on a daily base on ONBASE db. Apply transcoding to complete action code message.
+        Also offer flexibility to create ONBASE DIP index file, and zip audit log before archiving to gain storage space
 
     .INPUTS
-        Description of objects that can be piped to the script.
+        Configuration file 
 
     .OUTPUTS
-        Description of objects that are output by the script.
+        CSV log file
+        CSV Dip index file
+        Audit logs in text or zip format
 
     .EXAMPLE
-        Example of how to run the script.
+        .\Get-TransactionLogs.ps1 -conf .\dev.conf -dip -zip
 
     .LINK
         Links to further documentation.
 
     .NOTES
         Detail on what the script does, if this is needed.
+
+    Configuration file template
+
+    <conf>
+        <db>
+            <sqlServerInstance>CLUSQL01TSTBASE</sqlServerInstance>
+            <database>ONBASE_DEV</database>
+            <userName>obdev_admin</userName>
+            <password>&lt;?lM`xS$&amp;e*y$mAf5&amp;G&gt;</password>
+        </db>
+        <msgtbl_path>C:\Users\LD06974\OneDrive - Touring Club Suisse\03_DEV\06_GITHUB\TCS_AE\Projects\ONBASE_ExtractTransLog\msg_catalog.csv</msgtbl_path>
+        <output_path>C:\Users\LD06974\OneDrive - Touring Club Suisse\03_DEV\06_GITHUB\TCS_AE\Projects\ONBASE_ExtractTransLog\data\</output_path>
+        <targetDocType>158</targetDocType>
+    </conf>    
+
+    Message table format is four columns CSV file and based on official Hyland documentation Onbase v22.3 :
+    - Log Message	
+    - Table	
+    - actionnum	
+    - subactionnum
 
     #>
 #----------------------------------------------------------------------------------------------------------------------------------
@@ -62,6 +85,22 @@ param (
         )
     ] 
     [Alias('d')] [switch]$dip,
+
+    [Parameter(
+        Mandatory = $false,
+        ValueFromPipelineByPropertyName = $false,
+        Position = 4
+        )
+    ] 
+    [Alias('noTranscoding','noTransco','nt')] [switch]$raw,
+ 
+    [Parameter(
+        Mandatory = $false,
+        ValueFromPipelineByPropertyName = $false,
+        Position = 5
+        )
+    ] 
+    [Alias('z')] [switch]$zip,
  
     # help switch
     [switch] $help
@@ -77,7 +116,6 @@ param (
         Setup logging facilities by defining log path and default levels.
         Create log instance
 #>
-
 
 BEGIN {
     #----------------------------------------------------------------------------------------------------------------------------------
@@ -106,6 +144,8 @@ BEGIN {
     # Set log default and minum level for logging (ideally DEBUG when having trouble)
     Set-DefaultLogLevel -Level "INFO"
     Set-MinLogLevel -Level "DEBUG"
+
+    $DebugPreference = "SilentlyContinue"
 }
 
 PROCESS {
@@ -133,8 +173,7 @@ PROCESS {
     $VERSION      = "0.1"
     $AUTHOR       = "DLA"
     $SCRIPT_DATE  = "20230217"
-
-    
+        
     #----------------------------------------------------------------------------------------------------------------------------------
     #                                                 F U N C T I O N S 
     #----------------------------------------------------------------------------------------------------------------------------------
@@ -193,6 +232,11 @@ PROCESS {
     
     }
  
+    #..................................................................................................................................
+    # Function : Get-FingerPrint
+    #..................................................................................................................................
+    # Compute a finger print for a file or a PSObject
+    #..................................................................................................................................
     function Get-FingerPrint {
         param (
             [Parameter(
@@ -235,11 +279,13 @@ PROCESS {
         "Extract and prepare ONBASE transaction logs for archiving."
         " "
         "Options : "
-        "-Config    Configuration file name"
-        "-startDate "
-        "-endDate   "
-        "-DIP       Generate DIP index file"
-        "-Help      Display command help"
+        "-Config            Configuration file name"
+        "-startDate         Start extraction date"
+        "-endDate           End extraction date"
+        "-DIP               Generate DIP index file"
+        "-noTranscoding     No message transcoding / completion"
+        "-zip               Create output zip archive"
+        "-Help              Display command help"
         Write-Host $SEP_L1
         exit 0;
     }
@@ -255,8 +301,6 @@ PROCESS {
             as with the help and documentation for a function or script.
     #>
     
-    # Quick comment
-    
     # Script infp
     Write-Host $SEP_L1
     Write-Host ($MyInvocation.MyCommand.Name + " v" + $VERSION)
@@ -268,8 +312,6 @@ PROCESS {
         exit 0;
     }
     
-
-
     # 1 - Load script config file
     try {
         [XML]$conf = Get-Content $config_path
@@ -283,7 +325,7 @@ PROCESS {
     }
 
     # 2 -  Send SQL query to extract transaction logs
-    Write-Host ("Executing extract SQL query....")
+    Write-Host ("Build and execute Onbase audit trails extraction query...")
     $date      = (get-date -format "yyyyMMdd_HHmmss")
     if (-not $startDate) { 
         $startDate = (get-date)
@@ -294,24 +336,69 @@ PROCESS {
     $strEndDate   = '{0:yyyy-dd-MM 23:59:59}' -f $endDate
 
     $outputFileName = $conf.conf.output_path + "\" + $date + "_transactionLog.txt" 
-    $query = "SELECT *  FROM [hsi].[transactionxlog] WHERE [hsi].[transactionxlog].logdate >= '" + $strStartDate + "' AND [hsi].[transactionxlog].logdate < '" + $strEndDate + "';"
-    Write-Host ("Query : " + $query)
+    $query = "SELECT TMaster.transactionnum `
+        ,TMaster.tmessage`
+        ,TMaster.action`
+        ,TMaster.logdate`
+        ,TMaster.num`
+        ,TMaster.usernum`
+        ,TUser.username`
+        ,TMaster.itemnum`
+        ,TMaster.docrevnum`
+        ,TMaster.registernum`
+        ,TMaster.actionnum`
+        ,TMaster.subactionnum`
+        ,TMaster.severityflag`
+        ,TMaster.tracelvl`
+        ,TMaster.extrainfo1`
+        ,TMaster.extrainfo2`
+    FROM hsi.transactionxlog AS TMaster`
+    INNER JOIN hsi.useraccount AS TUser ON TUser.usernum = TMaster.usernum`
+    WHERE TMaster.logdate >= '" + $strStartDate + "' AND TMaster.logdate < '" + $strEndDate + "';"
+
+    Write-Debug ("Query : " + $query)
     $response = GenericSQLQuery -sql $query
-    #$response | ForEach-Object { $fingerprint = Get-FingerPrint -str $_; $_ | Add-Member -NotePropertyName fingerPrint -NotePropertyValue $fingerprint} 
+    Write-Host("Total record(s) extracted    : " + $response.Count + " item(s)")
+
+    # 3 - Trancode / complete log record message
+    if (-not $raw) { 
+        $trCount = 0
+        Write-Host "Apply transcoding on action/subaction message codes..."
+        $msgList = Import-Csv -path $conf.conf.msgtbl_path -Delimiter ";" -Header @('message','table','actionnum','subactionnum')
+        $response | ForEach-Object { 
+            $actionnum = $_.actionnum
+            $subactionnum = $_.subactionnum
+            $message = $msgList | Where-Object { (($_.actionnum -eq $actionnum) -and ($_.subactionnum -eq $subactionnum))} | Select-Object -ExpandProperty message  
+            $actionMessage = ($_.tmessage.Trim() -replace '\s{2,}|\"{2,}', '') + " - " + $message
+            Write-Debug ("Transcoding message action/subaction code (" + $_.actionnum + "-" + $_.subactionnum + ") as string message => '" + $actionMessage +"'" )
+            $_ | Add-Member -NotePropertyName tfullmessage -NotePropertyValue $actionMessage
+            $trCount++
+        }
+        Write-Host("Total record(s) transcoded   : " + $trCount + " item(s)") 
+    }
     $response | ConvertTo-Csv -Delimiter ";" | ForEach-Object {$_.Trim() -replace '\s{2,}|\"{2,}', ''} | Set-Content -path $outputFileName 
     
-    # If DIP is set, then we generate an CSV index for further import
-    if ($dip) {
-        $idxFileName =  $conf.conf.output_path + "\" + $date + "_DIP_Indexes.csv"
-        "Generate index file for DIP : " + $idxFileName
-        $idx = "ONBASE;ONBASE;INTERNE;AUD - Import Onbase transactions logs file;" + (get-date -format "yyyy-MM-dd") + ";" + (get-date -format "yyyy-MM-dd") + ";IT;;;CONFIDENTIAL;" + $conf.conf.targetDocType + ";" + $outputFileName
-        $idx | Set-Content -path $idxFileName
+    # 4 - Create zip archive file
+    if ($zip) {
+        Write-Host "Create archive file..."
+        Compress-Archive -Path $outputFileName -DestinationPath ($outputFileName + '.zip')
     }
 
-    # Compress-Archive -Path $outputFileName -DestinationPath ($outputFileName + '.zip')
-    Write-Host("Total record(s) extracted : " + $response.Count + " item(s)")
-    Write-Host("Output file               : " + $outputFileName)
-
+    # 5 - If DIP is set, then we generate an CSV index for further import
+    if ($dip) {
+        $idxFileName =  $conf.conf.output_path + "\" + $date + "_DIP_Indexes.csv"
+        "Generate index file for DIP  : " + $idxFileName
+        if ($zip) { 
+            $zipExt ='.zip'
+        }
+        $idx = "ONBASE;ONBASE;INTERNE;AUD - Import Onbase transactions logs file;" + (get-date -format "yyyy-MM-dd") + ";" + (get-date -format "yyyy-MM-dd") + ";IT;;;CONFIDENTIAL;" + $conf.conf.targetDocType + ";" + $outputFileName + $zipExt
+        $idx | Set-Content -path $idxFileName
+        if ($zip) {
+            Remove-Item -path $outputFileName
+        }
+    }
+    Write-Host("Output file                  : " + $outputFileName)
+    
     # Standard exit
     Write-Host $SEP_L1
     Stop-Log | Out-Null
